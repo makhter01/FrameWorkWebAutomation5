549a649d3a5a11d95ebf8260ad44df90be719958
makhter01 <mehzabinakhter@gmail.com>
2018-01-19 18:12:24 -0500

    this is my 3rd commit

--- Homedepot/src/test/java/testcases/GoogleExcellSheetTest.java
+++ Homedepot/src/test/java/testcases/GoogleExcellSheetTest.java
@@ -14,10 +14,13 @@ public class GoogleExcellSheetTest extends GoogleExcelSheetPages{
         String spreadsheetId = "1V4QjFNgr21KHvYPTbfscX0t9LDnE8lUrK07KttwLmUI";
         String range = "Sheet1!A2:C";
         List<String> actualItems = searchItemByName(spreadsheetId, range);
-        System.out.println(actualItems.get(i));
+        //System.out.println("actual: " + actualItems.get(i));
         List<List<Object>> expectedItems = getSpreadSheetRecords(spreadsheetId, range);
-        System.out.println(expectedItems.get(i));
+        //System.out.println("expected: " + expectedItems.get(i));
         for (List row : expectedItems) {
+            System.out.println("actual: " + actualItems.get(i));
+            System.out.println("expected: " + expectedItems.get(i));
+
             Assert.assertTrue(actualItems.get(i).contains(row.get(2).toString()));
 
             System.out.println(expectedItems.get(i) + ": Search - Passed");
